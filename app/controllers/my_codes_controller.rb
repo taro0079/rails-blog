400e6a39fa798e7634d46a8cc2e8e2b5a2291075
Taro Morita <morita0079@gmail.com>
2024-09-13 09:58:23 +0900

    highlight js

--- app/controllers/my_codes_controller.rb
+++ app/controllers/my_codes_controller.rb
@@ -3,12 +3,13 @@ require "uri"
 
 class MyCodesController < ApplicationController
   def index
-    uri = URI.parse("https://api.github.com/repos/taro0079/yadm-dotfiles/contents/.vimrc")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    headers = { "Accept"=> "application/vnd.github.v3.raw" }
-    response = http.get(uri.request_uri, headers)
-    @my_code = response.body
+    # uri = URI.parse("https://api.github.com/repos/taro0079/yadm-dotfiles/contents/.vimrc")
+    # http = Net::HTTP.new(uri.host, uri.port)
+    # http.use_ssl = true
+    # headers = { "Accept"=> "application/vnd.github.v3.raw" }
+    # response = http.get(uri.request_uri, headers)
+    # @my_code = response.body
+    @my_code = "test test"
   end
 
   private
